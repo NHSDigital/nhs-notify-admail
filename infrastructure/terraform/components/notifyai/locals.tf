d7e65f2fddef1eba817700860ee2d4c936983e6c
tomobkr <tomobkr@aol.com>
2025-07-11 10:35:11 +0000

    Removed redundant local variables

--- infrastructure/terraform/components/notifyai/locals.tf
+++ infrastructure/terraform/components/notifyai/locals.tf
@@ -1,7 +1,3 @@
 locals {
   prompt-file-name = "prompts.jsonl"
-  prompt-model-arn = "${var.prompt-model}"
-
-  evaluation-evaluator-model-identifier-arn = "${var.evaluation-evaluator-model-identifier}"
-  evaluation-inference-model-identifier-arn = "${var.evaluation-inference-model-identifier}"
 }
