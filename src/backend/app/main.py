ced19f2c493a07baae6e4cd239b025fd4e0885ec
ChristopherBacon <christopher.bacon1@nhs.net>
2025-07-28 16:34:42 +0100

    fix: deactivate the convert endpoint while changing to JWT

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -37,61 +37,61 @@ app.add_middleware(
     allow_headers=["*"],  # Allow all headers
 )
 
-@app.post("/convert")
-async def convert_file(
-    file: UploadFile = File(None)
-):
-    try:
-        if not file:
-            raise HTTPException(status_code=400, detail="Provide a file")
+# @app.post("/convert")
+# async def convert_file(
+#     file: UploadFile = File(None)
+# ):
+#     try:
+#         if not file:
+#             raise HTTPException(status_code=400, detail="Provide a file")
 
-        filename: Path = Path(file.filename)
-        file_type: str = filename.suffix
-        content: bytes = await file.read()
-        extracted_text = ""
+#         filename: Path = Path(file.filename)
+#         file_type: str = filename.suffix
+#         content: bytes = await file.read()
+#         extracted_text = ""
 
-        with open(f"{file.filename}", "wb") as f:  # saves to the build folder
-            f.write(content)
+#         with open(f"{file.filename}", "wb") as f:  # saves to the build folder
+#             f.write(content)
 
-        # not always 100% successful if pdfs are complicated or encoded in a way that it cannot parse
-        if file_type == ".pdf":
-            reader = pypdf.PdfReader(f"{file.filename}")
+#         # not always 100% successful if pdfs are complicated or encoded in a way that it cannot parse
+#         if file_type == ".pdf":
+#             reader = pypdf.PdfReader(f"{file.filename}")
 
-            for page in reader.pages:
-                text = page.extract_text()
-                if text:
-                    extracted_text += text + "\n"
+#             for page in reader.pages:
+#                 text = page.extract_text()
+#                 if text:
+#                     extracted_text += text + "\n"
 
-        else:
-            # Run Pandoc to convert the file from commandline
-            command = [
-                "pandoc",
-                file.filename,
-                "-o",
-                "file.txt",
-            ]
-            completed_process = subprocess.run(command, check=True, capture_output=True)
+#         else:
+#             # Run Pandoc to convert the file from commandline
+#             command = [
+#                 "pandoc",
+#                 file.filename,
+#                 "-o",
+#                 "file.txt",
+#             ]
+#             completed_process = subprocess.run(command, check=True, capture_output=True)
 
-            # Read the converted file
-            with open("file.txt", "rb") as f:
-                converted_data = f.read()
-                extracted_text = converted_data
+#             # Read the converted file
+#             with open("file.txt", "rb") as f:
+#                 converted_data = f.read()
+#                 extracted_text = converted_data
 
-            # prompt_resp = bedrock_call(extracted_text)
-            logger.info("We have the prompt response")
-            logger.info(extracted_text)
+#             # prompt_resp = bedrock_call(extracted_text)
+#             logger.info("We have the prompt response")
+#             logger.info(extracted_text)
 
-            try:
-                # finally remove file:
-                os.remove("file.txt")
-                os.remove(f"{file.filename}")
+#             try:
+#                 # finally remove file:
+#                 os.remove("file.txt")
+#                 os.remove(f"{file.filename}")
 
-                logger.info(
-                    f"File 'file.txt' and '{file.filename}'deleted successfully."
-                )
-            except Exception as e:
-                logger.error(e)
-        return extracted_text
-    except Exception as e:
-        logger.error(f"Server error: {str(e)}")
-        raise HTTPException(status_code=500, detail=str(e))
+#                 logger.info(
+#                     f"File 'file.txt' and '{file.filename}'deleted successfully."
+#                 )
+#             except Exception as e:
+#                 logger.error(e)
+#         return extracted_text
+#     except Exception as e:
+#         logger.error(f"Server error: {str(e)}")
+#         raise HTTPException(status_code=500, detail=str(e))
