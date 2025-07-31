50fb2b4cc8fba927678fcb857d5e5d5d82c67c29
tomobkr <tomobkr@aol.com>
2025-07-31 11:21:40 +0100

    Addded blank coverage file

---
+++ scripts/tests/coverage.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cd "$(git rev-parse --show-toplevel)"
+
+# This file is for you! Edit it to call your coverage suite. Note that the same
+# file will be called if you run it locally as if you run it on CI.
+# add in whatever is appropriate to your project.
