bc94da32b68d88dd640447eec94216dfef1afbcd
tomobkr <tomobkr@aol.com>
2025-07-31 11:29:46 +0100

    Added blank test files, for acceptance stage

--- scripts/tests/accessibility.sh
+++ scripts/tests/accessibility.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 cd "$(git rev-parse --show-toplevel)"
 
-# This file is for you! Edit it to call your test suite. Note that the same
+# This file is for you! Edit it to call your accessibility test suite. Note that the same
 # file will be called if you run it locally as if you run it on CI.
 # add in whatever is appropriate to your project.
