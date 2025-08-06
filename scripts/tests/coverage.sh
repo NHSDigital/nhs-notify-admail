8c10feb068d50b1a00047e347f788d4acb10735c
tomobkr <tomobkr@aol.com>
2025-08-06 17:33:01 +0100

    Updated paths to be more explicit, to see if the coverage report will map to what sonarcloud expects

--- scripts/tests/coverage.sh
+++ scripts/tests/coverage.sh
@@ -7,10 +7,10 @@ cd "$(git rev-parse --show-toplevel)"
 # This file is for you! Edit it to call your test suite. Note that the same
 # file will be called if you run it locally as if you run it on CI.
 # add in whatever is appropriate to your project.
-PYTHONPATH=src/backend/
+PYTHONPATH=src/backend
 find $PYTHONPATH -name "requirements.txt" -exec pip install -r {} \;
 python -m pytest \
-  $PYTHONPATH \
-  --cov=src/backend \
+  src/backend \
   --cov-config=scripts/config/.coveragerc \
-  --cov-report=xml
+  --cov-report=xml \
+  --cov=.
