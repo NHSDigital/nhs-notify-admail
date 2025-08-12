c8b435cd355dc7adf8f5e1121fac5ddae503f254
ChristopherBacon <christopher.bacon1@nhs.net>
2025-08-12 11:51:41 +0100

    fix the data returned object so is parsible

--- src/frontend/notifai-uploader/src/components/FileUpload.js
+++ src/frontend/notifai-uploader/src/components/FileUpload.js
@@ -28,7 +28,8 @@ export default function FileUpload({ onFileUpload }) {
           }
         );
         setUploadStatus('Successfully Uploaded');
-        onFileUpload(response.data.extracted_text); // Call the parent callback
+
+        onFileUpload(response.data); // Call the parent callback
         setTimeout(() => setUploadStatus(''), 3000); // Clear status after 3 seconds
       } catch (error) {
         console.error('Upload failed:', error);
