6b90181a29ec0f5a9d94e695f863e577271900b5
ChristopherBacon <christopher.bacon1@nhs.net>
2025-08-14 10:42:18 +0100

    fix: incorrect import for useAuth in ConvertAPI

--- src/frontend/notifai-uploader/src/api/ConvertAPI.js
+++ src/frontend/notifai-uploader/src/api/ConvertAPI.js
@@ -1,6 +1,6 @@
 import { useMemo } from 'react';
 import axios from 'axios';
-import { useAuth } from './AuthContext';
+import { useAuth } from '../components/AuthContext';
 
 export function useConvertAPI() {
   const { user, refreshSession } = useAuth();
