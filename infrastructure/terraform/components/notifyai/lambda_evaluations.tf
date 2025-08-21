1e199280334c6739ecc87ff209990bf3e8f04fc8
ChristopherBacon <christopher.bacon1@nhs.net>
2025-08-21 14:42:14 +0100

    fix: update handler for aws lambda function

--- infrastructure/terraform/components/notifyai/lambda_evaluations.tf
+++ infrastructure/terraform/components/notifyai/lambda_evaluations.tf
@@ -12,7 +12,7 @@ resource "aws_lambda_function" "bedrock-evaluations" {
   function_name    = local.lambda_name
   filename         = data.archive_file.docx_to_string_file.output_path
   role             = aws_iam_role.iam_for_lambda.arn
-  handler          = "main.lambda_handler" // change once converted the converter to lambda
+  handler          = "bedrock_evaluation_lambda.lambda_handler"
   source_code_hash = data.archive_file.docx_to_string_file.output_base64sha256
   runtime          = "python3.12"
   timeout          = 30
