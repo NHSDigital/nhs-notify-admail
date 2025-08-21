ddb6143b16b0b4381f76a739773ae1b6603e6962
ChristopherBacon <christopher.bacon1@nhs.net>
2025-08-21 14:32:28 +0100

    feat: automated evaluations run every three days

---
+++ infrastructure/terraform/components/notifyai/lambda_evaluations.tf
@@ -0,0 +1,47 @@
+locals {
+  lambda_name = "${local.csi}-bedrock-evaluations"
+}
+
+data "archive_file" "docx_to_string_file" {
+  type        = "zip"
+  source_dir  = "../../../../tools/evaluation-runner"
+  output_path = "lambda_evaluations.zip"
+}
+
+resource "aws_lambda_function" "bedrock-evaluations" {
+  function_name    = local.lambda_name
+  filename         = data.archive_file.docx_to_string_file.output_path
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "main.lambda_handler" // change once converted the converter to lambda
+  source_code_hash = data.archive_file.docx_to_string_file.output_base64sha256
+  runtime          = "python3.12"
+  timeout          = 30
+
+  environment {
+    variables = {
+      env_evaluator_model_identifier = var.evaluation_evaluator_model_identifier
+      env_generator_model_identifier = var.evaluation_inference_model_identifier
+      env_role_arn                   = aws_iam_role.iam_for_bedrock_evaluation.arn
+      env_region                     = var.region
+      env_input_prompt_s3_uri        = bedrock_evaluation_prompt_dataset_s3_uri
+      env_results_s3_uri             = bedrock_evaluation_results_s3_uri
+    }
+  }
+}
+
+module "eventbridge" {
+  source = "terraform-aws-modules/eventbridge/aws"
+  version = 1.0.0
+
+  bus_name = "example" # "default" bus already support schedule_expression in rules
+
+  schedules = {
+    lambda-cron = {
+      description         = "Trigger for Lambda evaluations"
+      schedule_expression = "rate(3 days)"
+      timezone            = "Europe/London"
+      arn                 = aws_lambda_function.bedrock-evaluations.arn
+      input               = jsonencode({ "job" : "cron-by-rate" })
+    }
+  }
+}
---
+++ tools/evaluation-runner/bedrock_evaluation_lambda.py
@@ -0,0 +1,44 @@
+import os
+import json
+import logging
+from datetime import datetime
+from bedrock_evaluation_service import BedrockEvaluator
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def lambda_handler(event, context):
+    try:
+        evaluator_model = os.environ["env_evaluator_model_identifier"]
+        generator_model = os.environ["env_generator_model_identifier"]
+        role_arn = os.environ["env_role_arn"]
+        aws_region = os.environ["env_region"]
+        input_s3_uri = os.environ["env_input_prompt_s3_uri"]
+        output_s3_uri = os.environ["env_results_s3_uri"]
+
+        evaluator = BedrockEvaluator(region=aws_region, role_arn=role_arn)
+        result = evaluator.run_evaluation_job(
+            evaluator_model=evaluator_model,
+            generator_model=generator_model,
+            input_s3_uri=input_s3_uri,
+            output_s3_uri=output_s3_uri,
+        )
+        return {"statusCode": 200, "body": json.dumps(result)}
+
+    except KeyError as e:
+        logger.error("Missing environment variable: %s", e)
+        return {
+            "statusCode": 400,
+            "body": json.dumps(
+                {"error": f"Configuration error: Missing environment variable {e}"}
+            ),
+        }
+    except Exception as e:
+        logger.error("An unexpected error occurred: %s", e)
+        return {
+            "statusCode": 500,
+            "body": json.dumps(
+                {"error": "An internal error occurred. Check logs for details."}
+            ),
+        }
