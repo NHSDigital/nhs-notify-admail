b45c7ef24c83f8277d947e1cf1ebb95557ab923a
tomobkr <tomobkr@aol.com>
2025-08-22 15:16:52 +0100

    Moved about imports

--- src/backend/bedrock-evaluations-runner/bedrock_evaluation_local_runner.py
+++ src/backend/bedrock-evaluations-runner/bedrock_evaluation_local_runner.py
@@ -2,14 +2,12 @@ import os
 import json
 import logging
 from bedrock_evaluation_lambda import lambda_handler
+from dotenv import load_dotenv
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
-
 logger = logging.getLogger(__name__)
-
-from dotenv import load_dotenv
 
 load_dotenv()
 
