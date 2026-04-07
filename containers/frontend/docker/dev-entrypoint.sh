1104454f76f98fa0d1680697a26e312192335c34
Joe Stringer <joe.stringer5@nhs.net>
2026-04-07 16:46:41 +0100

    feat: logging for local running frontend

--- containers/frontend/docker/dev-entrypoint.sh
+++ containers/frontend/docker/dev-entrypoint.sh
@@ -13,4 +13,11 @@ export HOST=0.0.0.0
 # on Docker bind mounts (macOS/Windows hosts do not propagate inotify events)
 export WATCHPACK_POLLING=true
 
-exec npx react-scripts start
+# Enable access logs output
+export GENERATE_SOURCEMAP=true
+export BROWSER=none
+export TSC_COMPILE_ON_ERROR=true
+export REACT_APP_ACCESS_LOGS=true
+export DEBUG=express:*,connect:*
+
+exec npx react-scripts start 2>&1 | tee /var/log/react-access.log
