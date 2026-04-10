7aa8682367fb9cd0c12142257c558033993c3e46
Joe Stringer <joe.stringer5@nhs.net>
2026-04-10 11:14:11 +0100

    todo: integration test auth

--- scripts/tests/integration.sh
+++ scripts/tests/integration.sh
@@ -7,4 +7,5 @@ cd "$(git rev-parse --show-toplevel)"
 npm install
 npx --yes playwright install --with-deps > /dev/null
 
-npm run test:integration --workspace tests/playwright
+# TODO: CM-00000 - enable integration tests once Cognito access for tests resolved
+# npm run test:integration --workspace tests/playwright
